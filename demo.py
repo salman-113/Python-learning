052f3c6bf917277782eaee559eaf405410510b94
salman <mrsalmanxzs@gmail.com>
2025-10-06 11:45:33 +0530

    repush

--- demo.py
+++ demo.py
@@ -256,3 +256,104 @@
 # z = [1, 2, 3]
 # print(x is y)     # is → same memory object aano? # output: True
 # print(x is not z) # is not → different object aano? # output: True
+
+
+# # Write if-else to print "Even" or "Odd"
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+# Write if-elif-else to print "Positive", "Negative", or "Zero"
+# number = 0
+# if number >= 0:
+#     print("Positive")
+# else:
+#     print("Negative")
+
+
+# Write if-elif-else:
+# >=90 → "Grade A", >=75 → "Grade B", >=60 → "Grade C", else → "Fail"
+# marks = 85
+# if marks >= 90:
+#     print("Gradel A")
+# elif marks >= 75:
+#     print("Gradel B")
+# elif marks >= 60:
+#     print("Gradel C")
+# else:
+#     print("Fail")
+
+
+
+# Write match-case to print:
+# Monday → "Start of week"
+# Friday → "End of week"
+# _ → "Midweek"
+# day = "Monday"
+# match day:
+#     case "Monday":
+#         print("Start of week")
+#     case "Friday":
+#         print("End of week")
+#     case _:
+#         print("Midweek")
+
+
+
+# # Write if-else to print "Teenager" if 13-19, else "Not a Teenager"
+# age = 15
+# if age >= 13 and age <= 19:
+#     print("Teenager")
+# else:
+#     print("Not teenage") 
+
+
+
+# if num < 20 → "Small"
+# elif num <= 50 → "Medium"
+# else → "Large"
+
+# num = 50
+# if num < 20:
+#     print("small")
+# elif num <= 50:
+#     print("medium")
+# else:
+#     print("large")
+
+
+
+# match-case to print:
+# "Apple" → "Red"
+# "Banana" → "Yellow"
+# _ → "Unknown"
+
+# fruit = "apple"
+# match fruit:
+#     case "apple":
+#         print("red")
+#     case "Banana":
+#         print("yellow")
+#     case _:
+#         print("unknown")
+
+
+# for i in range(50):
+#     print(i)
+
+# a = ["apple", "baanaana", "grapes"]
+# for b in a:
+#     print(b)
+
+# a = 1
+
+# while a <= 8:
+#     print(a)
+#     a +=1
+
+
+for i in range(10):
+    pass
+    print(i)
