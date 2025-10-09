bb4f503ade1d2b4427c18814cd49d046c62bf556
salman <mrsalmanxzs@gmail.com>
2025-10-09 12:21:05 +0530

    repush

--- my_oops.py
+++ my_oops.py
@@ -16,9 +16,28 @@
 
 
 
-class Student:
-    def study(self):
-        print("The student is studying")
 
-s1 = Student()
-s1.study()
+# class Student:
+#     def __init__(self,book):
+#         self.book = book
+
+#     def study(self):
+#         print(f"The student is studying {self.book}")
+
+# s1 = Student("Textbook")
+# s1.study()
+# s1.study()
+
+
+
+
+class Developer:
+    def __init__(self,name,stack):
+        self.name = name
+        self.stack = stack
+
+    def Display(self):
+        print(f"The {self.name} is a {self.stack} Developer")
+
+dev = Developer("Salman", "Python Full-stack") 
+dev.Display()       
