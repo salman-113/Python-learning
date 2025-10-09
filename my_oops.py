3d1af2268efbedbd7936d6797545cf7a8c409eee
salman <mrsalmanxzs@gmail.com>
2025-10-09 11:19:31 +0530

    repush

--- my_oops.py
+++ my_oops.py
@@ -1,1 +1,24 @@
 # oops concept learning 
+
+# class Car:
+#     def __init__(self, brand, color):
+#         self.brand = brand
+#         self.color = color
+
+#     def start(self):
+#         print(f"{self.color} {self.brand} car is starting......")
+
+# car1 = Car("Toyota", "Red")
+# car2 = Car("BMW", "Black")
+# car1.start() 
+# car2.start()
+# print(type(car1))
+
+
+
+class Student:
+    def study(self):
+        print("The student is studying")
+
+s1 = Student()
+s1.study()
