1e05fd6bce3b6cd78a7abdd261b9780c7c440b0c
salman <mrsalmanxzs@gmail.com>
2025-10-10 11:38:51 +0530

    repush

--- inheritance.py
+++ inheritance.py
@@ -51,3 +51,114 @@
 
 
 # ➡️ Each object calls its own version of the method:
+
+
+
+# 1. Single Inheritance
+
+# class Parent:
+#     def show_parent(self):
+#         print("This is parent class")
+
+# class Child(Parent):
+#     def show_child(self):
+#         print("This is child class")           
+
+# c = Child()
+# c.show_parent()
+# c.show_child()
+
+
+# 2. Multilevel Inheritance
+# Child → Parent → Grandparent (chain-like)
+
+# class GrandParent:
+#     def show_gp(self):
+#         print("This is GrandParent Class")
+
+# class Parent(GrandParent):
+#     def show_parent(self):
+#         print("This is Parent Class")
+
+# class Child(Parent):
+#     def show_child(self):
+#         print("This is Child class")     
+
+# c = Child()
+# c.show_gp()
+# c.show_parent()
+# c.show_child()
+
+
+
+# 3. Multiple Inheritance
+# Child inherits from many parents.
+
+# class Father:
+#     def feature1(self):
+#         print("Fuature from Father")
+
+# class Mother:
+#     def feature2(self):
+#         print("Feature from Mother")
+
+# class Child(Father, Mother):
+#     def feature3(self):
+#         print("Feature from Child")
+
+# c = Child()
+# c.feature1()
+# c.feature2()
+# c.feature3()
+
+
+
+# 4. Hierarchical Inheritance
+# Many children inherit from one parent.
+
+# class Parent:
+#     def show_parent(self):
+#         print("This is parent Class")
+
+# class Child1(Parent):
+#     def show_child1(self):
+#         print("This is Child 1")
+
+# class Child2(Parent):
+#     def show_child2(self):
+#         print("This is Child 2")
+
+# c1 = Child1()
+# c2 = Child2()
+
+# c1.show_parent()
+# c2.show_parent()
+
+
+
+# 5. Hybrid Inheritance
+# A mix of multiple and multilevel inheritance.
+
+# class A:
+#    def methodA(self):
+#       print("A class") 
+
+# class B(A):
+#    def methodB(self):
+#       print("B class")
+
+# class C(A):
+#    def methodC(self):
+#       print("C class")
+
+# class D(B, C):  # Hybrid: inherits from both B and C (which inherit from A)
+#    def methodD(self):
+#       print("D class")
+
+# obj = D()
+# obj.methodA()
+# obj.methodB()
+# obj.methodC()
+# obj.methodD()
+# print(D.mro())
+# print(D.__mro__) # used to chek order when there are multiple paths
