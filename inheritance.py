8c937687f467cb352bd5b44ce8280449583b3e6f
salman <mrsalmanxzs@gmail.com>
2025-10-10 10:10:40 +0530

    repush

--- inheritance.py
+++ inheritance.py
@@ -11,13 +11,43 @@
 # c1.show_child()
 
 
-class Parent:
-    def __init__(self):
-        print("parent class constructor")
+# class Parent:
+#     def __init__(self):
+#         print("parent class constructor")
 
-class Child(Parent):
-    def __init__(self):
-        super().__init__()
-        print("Child class constructor")
+# class Child(Parent):
+#     def __init__(self):
+#         super().__init__()
+#         print("Child class constructor")
 
-obj = Child()
+# obj = Child()
+
+
+
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def speake(self):
+#         return "Geniric animal sound"     # ➡️ Animal is the parent (base) class
+#                                           # ➡️ It has a method speake() (common for all animals).
+
+# class Dog(Animal):
+#     def speake(self):
+#         return "woof !"
+
+# class Cat(Animal):
+#     def speake(self):
+#         return "Meow !"    # ➡️ Both Dog and Cat override the parent method speake()
+#                            # ➡️ This is called method overriding — a part of polymorphism.
+
+# dog = Dog("Buddy")
+# cat = Cat("Whiskers")
+
+# print(dog.name)
+# print(dog.speake())
+# print(cat.name)
+# print(cat.speake())
+
+
+# ➡️ Each object calls its own version of the method:
