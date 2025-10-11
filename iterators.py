0d9b73d2fda73e234cfd92a87b143a2204dad019
salman <mrsalmanxzs@gmail.com>
2025-10-12 03:07:59 +0530

    repush

--- iterators.py
+++ iterators.py
@@ -5,22 +5,24 @@
 # print(next(it))
 
 
-class MyNumbers:
-    def __init__(self, limit):
-        self.limit = limit
-        self.num = 0  # start from 0
+# class MyNumbers:
+#     def __init__(self, limit):
+#         self.limit = limit
+#         self.num = 0  # start from 0
 
-    def __iter__(self):
-        return self   # returns the iterator object itself
+#     def __iter__(self):
+#         return self   # returns the iterator object itself
     
-    def __next__(self):
-        if self.num < self.limit:
-            current = self.num
-            self.num += 1
-            return current
-        else:
-            raise StopIteration  # no more items
+#     def __next__(self):
+#         if self.num < self.limit:
+#             current = self.num
+#             self.num += 1
+#             return current
+#         else:
+#             raise StopIteration  # no more items
         
-numbers = MyNumbers(11)
-for i in numbers:
-    print(i)
+# numbers = MyNumbers(11)
+# for i in numbers:
+#     print(i)
+
+
