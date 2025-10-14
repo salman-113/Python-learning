eda8df4a562d9bc6d1dd48bab0456b5167dbc13c
salman <mrsalmanxzs@gmail.com>
2025-10-14 21:30:09 +0530

    push

---
+++ Extra Topics/map.py
@@ -0,0 +1,19 @@
+# The map() function is used to apply a function to every item in a sequence (like a list, tuple, etc.).
+
+# numbers = [1, 2, 3, 4, 5]
+
+# # def square(nums):
+# #     return nums * nums
+
+# # result = map(square, numbers)
+# # print(list(numbers))
+
+
+# into = list(map(lambda n: n * 5, numbers))
+# print(into)
+
+
+# | Function   | Purpose                | Output                               |
+# | ---------- | ---------------------- | ------------------------------------ |
+# | `map()`    | Transforms all items   | Keeps all items (but modified)       |
+# | `filter()` | Selects specific items | Keeps only items that meet condition |
