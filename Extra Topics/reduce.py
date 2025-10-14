c80508bebe9ca8ed8afcd7186c67ca84c6b10013
salman <mrsalmanxzs@gmail.com>
2025-10-14 21:32:54 +0530

    push

---
+++ Extra Topics/reduce.py
@@ -0,0 +1,20 @@
+# The reduce() function is used to reduce a list (or any iterable) into a single value by applying a function repeatedly to its elements.
+
+# Syntax:
+# from functools import reduce
+# reduce(function, sequence)
+
+# from functools import reduce
+
+# numbers = [1, 2, 3, 4, 5]
+
+# result = reduce(lambda x, y : x + y, numbers)
+# print(result)
+
+
+
+# | Function   | Purpose                       | Returns                      |
+# | ---------- | ----------------------------- | ---------------------------- |
+# | `map()`    | Transform each element        | List/iterator of same length |
+# | `filter()` | Select some elements          | List/iterator (shorter)      |
+# | `reduce()` | Combine all elements into one | Single value                 |
