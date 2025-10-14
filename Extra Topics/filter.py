9055d1e1c517127f7fd99b848cbc176963572a31
salman <mrsalmanxzs@gmail.com>
2025-10-14 21:24:15 +0530

    push

---
+++ Extra Topics/filter.py
@@ -0,0 +1,13 @@
+# In Python, the filter() function is used to filter out (select) elements from a sequence (like a list, tuple, etc.) based on a condition.
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# def even(nums):
+#     return nums % 2 == 0
+# result = filter(even, numbers)
+# print(list(result))
+
+# using lambda for easy
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# even_numbers = list(filter(lambda n: n % 2 == 0, numbers))
+# print(even_numbers)
