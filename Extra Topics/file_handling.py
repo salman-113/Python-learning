d36fe5168b81d3047d5cffc1d7ad7aca43367a73
salman <mrsalmanxzs@gmail.com>
2025-10-14 21:16:33 +0530

    push

--- Extra Topics/file_handling.py
+++ Extra Topics/file_handling.py
@@ -12,8 +12,50 @@
 # f.write("Name: Salman\nScore: 95")
 # f.close()
 
-f = open("marks.txt", "r")
-data = f.read()
-print(data)
-f.close()
+# f2 = open("marks.txt", "r" )
+# data = f2.read()
+# print(data)
+# f2.close()
 
+# f1 = open("marks.txt", "a")
+# f1.write("I am writing new thing for study\n")
+# f1.close()
+
+# 1️⃣ Write to a file (creates new file or overwrites existing one)
+
+# f = open("myfile.txt", "w")
+# f.write("Hello! This is my first file.\n")
+# f.write("I am learning file handling in Pyhton.\n")
+# f.close()
+
+# # 2️⃣ Read the file content
+
+f1 = open("myfile.txt", "r")
+# data = f1.read()
+# print(data)
+# f1.close()
+
+# # 3️⃣ Append more data to the file
+
+# f2 = open("myfile.txt", "a")
+# f2.write("This is now i added for append learnig\n")
+# f2.close()
+
+
+# 4️⃣ Use 'with open()' for safe reading
+
+# with open("myfile.txt", "r") as f:
+#     for line in f:
+#         print(line.strip())
+
+
+# line1 = f1.readline()
+# print(line1.strip())
+# f1.close()
+
+# lines = f1.readlines()
+# f1.close()
+# print(lines)
+
+# for line in lines:
+#     print(line.strip())
