995bf9d1bda0eda1928b3e7c50d71f711f3c0c34
salman <mrsalmanxzs@gmail.com>
2025-10-15 16:15:13 +0530

    repush

---
+++ Reaview Prepration/problem.py
@@ -0,0 +1,27 @@
+# 1. Reverse string
+# Problem: Reverse "hello" without slicing
+
+# s = "HELLO"
+# rev = ""
+# for i in s:
+#     rev = i + rev
+
+# print(rev)
+
+
+# 2. Count Vowels
+# Problem: Count vowels in "programming".
+
+
+# s = "Programming"
+# vowels = "aeiou"
+# count = 0
+# for i in s:
+#     if i in vowels:
+#         count += 1
+
+# print("Vowel Count:", count)
+
+
+
+
