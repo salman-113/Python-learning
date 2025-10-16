ffdeeb8ad8ee0c7c2c0f5c1885f92240dd5bc969
salman <mrsalmanxzs@gmail.com>
2025-10-16 16:10:22 +0530

    repush

---
+++ Reaview Prepration/review_qustion.py
@@ -0,0 +1,28 @@
+# my_dict1 = {
+#     "a": 1, "b": 2, "c": 3, "d": 4
+# }
+
+# my_dict2 = {
+#     "e": 5, "f": 6, "g": 7, "h": 8
+# }
+
+# my_dict1.update(my_dict2)
+# print(my_dict1)
+
+# def capital(func):
+#     def wrapper(first, last):
+#         result = func(first, last)
+#         return result.upper()
+#     return wrapper
+
+# @capital
+# def names(first, last):
+#     return first + last
+# print(names("salman", "faris"))
+
+
+# names = lambda first, last: first + last
+# print(names("salman", "faris")) 
+    
+
+
