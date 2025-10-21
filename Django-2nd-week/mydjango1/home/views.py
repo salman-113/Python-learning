97f1db2136e184ef44bf27d157c68944d9010cbb
salman <mrsalmanxzs@gmail.com>
2025-10-21 17:01:27 +0530

    repush

--- Django-2nd-week/mydjango1/home/views.py
+++ Django-2nd-week/mydjango1/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("This is Home page")
+
+def about(request):
+    return HttpResponse("This is About page")
