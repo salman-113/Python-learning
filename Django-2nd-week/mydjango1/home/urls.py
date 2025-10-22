202d637d7337e8931da238d6cd595dc949c95830
salman <mrsalmanxzs@gmail.com>
2025-10-22 14:42:29 +0530

    repush

--- Django-2nd-week/mydjango1/home/urls.py
+++ Django-2nd-week/mydjango1/home/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('about/', views.about),
-
+    path('about', views.about),
+    path('booking', views.booking),
+    path('doctors', views.doctors),
+    path('contact', views.contact),
+    path('department', views.department),
 ]
--- Django-2nd-week/mydjango1/home/views.py
+++ Django-2nd-week/mydjango1/home/views.py
@@ -4,7 +4,29 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is Home page")
+
+    # person = {
+    #     'name' : 'Salman',
+    #     'age' : 20,
+    #     'place' : 'Calicut'
+    # }
+
+    numbers = {
+        'num1' : 10
+    }
+    return render(request, 'index.html', numbers)
 
 def about(request):
-    return HttpResponse("This is About page")
+    return render(request, 'about.html')
+
+def booking(request):
+    return render(request, 'booking.html')
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def department(request):
+    return render(request, 'department.html')
